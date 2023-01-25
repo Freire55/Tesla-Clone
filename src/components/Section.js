0486968c637327d322de16f3ac8be8bc4a7eca18
Tomás <noronha.t@sapo.pt>
2023-01-25 23:27:33 +0000

    Tesla Sections

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,18 +1,46 @@
 import React from 'react'
 import styled from 'styled-components'
+// import './Section.scss'
+import Image from './images/down-arrow.svg'
 
-function Section() {
+
+function Section({ title, description, backgroundImg, rightBtnText, leftBtnText}) {
   return (
-    <Wrap>
-        Section
+    <>
+    <Wrap bgImage={backgroundImg}>
+        <div className='itemtext'>
+            <h1>{title}</h1>
+            <p>{description}</p>
+        </div>
+        <div className='buttons'>
+        <div className='buttongroup'>
+            <div className='leftbutton'>
+                {leftBtnText}
+            </div>
+            {rightBtnText && 
+            <div className='rightbutton'>
+                {rightBtnText}
+            </div>
+            }
+        </div>
+        <img className='downarrow' src={Image} alt="arrow" />
+        </div>
     </Wrap>
+    </>
   )
 }
 
 export default Section
 
-const Wrap = styled.div `
+const Wrap = styled.div` 
+    background-size: cover;
     width: 100vw;
+    background-position: center;
+    background-repeat: no-repeat;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
     height: 100vh;
-    background-image: url('/tesla-images/model-s.jpg')
+    background-image: ${props => `url(${props.bgImage})`};
 `
